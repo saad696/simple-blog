2ed78ff3b4c4e8bb277c06ec6816c2173c64740c
saad696 <shksaad911@gmail.com>
2023-08-01 07:05:35 +0530

    created event buus

---
+++ server/eventBus/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/events', (req, res) => {
+    const event = req.body;
+
+    axios.post('http://locachost:4000/events', event);
+    axios.post('http://locachost:4001/events', event);
+    axios.post('http://locachost:4002/events', event);
+
+    res.status(200).send({ status: 'OK' });
+});
+
+app.listen(4005, () => {
+    console.log("Event Bus running on port:4005")
+})
